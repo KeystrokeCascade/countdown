87876be7403162bdb45765f0aeb065fb8a9d9d3e
KeystrokeCascade <haydenseen1@gmail.com>
2018-04-14 22:38:19 +1000

    Takes window h&w

--- schedule/app.js
+++ schedule/app.js
@@ -1,11 +1,17 @@
 //Collects the aspect ratio of the device
-var screen_x = screen.width;
-var screen_y = screen.height;
+var screen_x = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var screen_y = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
 var aspect_ratio = screen_x / screen_y
 
 //generates the url for derpibooru
 var random = Math.floor((Math.random() * 999999999999999999) + 0);
-var url = "https://derpibooru.org/search.json?q=score.gte%3A250%2C+Aspect_ratio%3A" + aspect_ratio + "~0.01%2C+NOT+animated%2C+NOT+absurd+res%2C+NOT+watermark%2C+NOT+eqg%2C+NOT+anthro%2C+NOT+human%2C+NOT+sfm&sf=random%3A" + random + "&filter_id=56027"
+var url = "https://derpibooru.org/search.json?q=score.gte%3A250%2C+Aspect_ratio%3A" + aspect_ratio + "~0.1%2C+NOT+text%2C+NOT+animated%2C+NOT+absurd+res%2C+NOT+watermark%2C+NOT+eqg%2C+NOT+anthro%2C+NOT+human%2C+NOT+sfm&sf=random%3A" + random + "&filter_id=56027"
 
 //gets an image from derpibooru  randomly with that aspect ratio
 var xmlhttp = new XMLHttpRequest();
@@ -14,11 +20,17 @@ xmlhttp.onreadystatechange = function() {
         var myObj = JSON.parse(this.responseText);
         var array_len = myObj.search.length;
         var image_num = Math.floor((Math.random() * array_len) + 0);
+		if (myObj.search[image_num] == undefined) {
+			url = "https://derpibooru.org/search.json?q=landscape%2C+background&sf=random%3A" + random
+			xmlhttp.open("GET", url, true);
+			xmlhttp.send();
+			return
+		}
         var id = myObj.search[image_num].id;
         document.getElementById("id").href = "https://derpibooru.org/" + id;
         var image = myObj.search[image_num].image;
         document.documentElement.style.backgroundImage = "url(https:" + image + ")";
-    }
+    };
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
@@ -27,7 +39,7 @@ xmlhttp.send();
 function toTimeZone(time, zone) {
     var format = 'HH:mm';
     return moment(time, format).tz(zone).format(format)
-}
+};
 
 //Runs it when loaded and then every minute
 schedule();
@@ -35,8 +47,8 @@ function timer() {
   var seconds = new Date().getSeconds();
   if (seconds == 0) {
     schedule();
-  }
-}
+  };
+};
 setInterval(timer,1000);
 
 //Takes the time and date and calculates the event on
@@ -66,8 +78,7 @@ function schedule() {
         agenda = "at home"
       } else if (time >= 2201 && time <= 2400) {
         agenda = "sleeping"
-      }
-      ;
+      };
 
 //Monday
   } else if (day == "Monday") {
@@ -91,8 +102,7 @@ function schedule() {
         agenda = "at home"
       } else if (time >= 2201 && time <= 2400) {
         agenda = "sleeping"
-      }
-      ;
+      };
 
 //Tuesday
   } else if (day == "Tuesday") {
@@ -118,8 +128,7 @@ function schedule() {
         agenda = "at home"
       } else if (time >= 2201 && time <= 2400) {
         agenda = "sleeping"
-      }
-      ;
+      };
 
 //Wednesday
   } else if (day == "Wednesday") {
@@ -143,8 +152,8 @@ function schedule() {
         agenda = "at home"
       } else if (time >= 2201 && time <= 2400) {
         agenda = "sleeping"
-      }
-      ;
+      };
+	  
 //Thursday
   } else if (day == "Thursday") {
       if (time >= 0 && time <= 745) {
@@ -167,8 +176,7 @@ function schedule() {
         agenda = "at home"
       } else if (time >= 2201 && time <= 2400) {
         agenda = "sleeping"
-      }
-      ;
+      };
 
 //Friday
   } else if (day == "Friday") {
@@ -194,8 +202,8 @@ function schedule() {
         agenda = "at home"
       } else if (time >= 2201 && time <= 2400) {
         agenda = "sleeping"
-      }
-      ;
+      };
+
 //Saturday
   } else if (day == "Saturday") {
       if (time >= 0 && time <= 800) {
@@ -204,9 +212,8 @@ function schedule() {
         agenda = "at home"
       } else if (time >= 2331 && time <= 2400) {
         agenda = "sleeping"
-      }
-      ;
-  }
+      };
+  };
   //prints the current agenda
   document.getElementById("schedule").textContent = agenda;
-}
+};
