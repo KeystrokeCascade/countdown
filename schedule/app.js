ef779c46f6b8374a2559d7573ddb06f47c43f2db
KeystrokeCascade <haydenseen1@gmail.com>
2018-04-15 16:27:12 +1000

    pretty please?

--- schedule/app.js
+++ schedule/app.js
@@ -60,14 +60,12 @@ var aspect_ratio = screen_x / screen_y
 
 //generates the url for derpibooru
 if (lewdness == "Lewd") {
-	var lewd = "%2C+explicit+%7C%7C+suggestive%2C+NOT+watersports%2C+NOT+scat%2C+NOT+hyper%2C+NOT+foalcon"
+	var lewd = "%2C+%28explicit+%7C%7C+suggestive+%7C%7C+questionable%29%2C+NOT+grimdark%2C+NOT+grotesque%2C+NOT+semi-grimdark%2C+NOT+watersports%2C+NOT+scat%2C+NOT+hyper%2C+NOT+foalcon"
 } else {
 	var lewd = "%2C+safe%2C+NOT+anthro"
 }
 var random = Math.floor((Math.random() * 999999999999999999) + 0);
-var url = "https://derpibooru.org/search.json?q=score.gte%3A250%2C+Aspect_ratio%3A" + aspect_ratio + "~0.1" + lewd + "%2C+NOT+text%2C+NOT+comic%2C+NOT+meme%2C+NOT+screencap%2C+NOT+edit+screencap%2C+NOT+animated%2C+NOT+comparison%2C+NOT+absurd+res%2C+NOT+watermark%2C+NOT+eqg%2C+NOT+human%2C+NOT+sfm&sf=random%3A" + random + "&filter_id=56027"
-
-console.log(url)
+var url = "https://derpibooru.org/search.json?q=score.gte%3A250%2C+Aspect_ratio%3A" + aspect_ratio + "~0.1" + lewd + "%2C+NOT+traditional+art%2C+NOT+text%2C+NOT+comic%2C+NOT+meme%2C+NOT+screencap%2C+NOT+edit+screencap%2C+NOT+animated%2C+NOT+comparison%2C+NOT+absurd+res%2C+NOT+watermark%2C+NOT+eqg%2C+NOT+human%2C+NOT+sfm&sf=random%3A" + random + "&filter_id=56027"
 
 //gets an image from derpibooru  randomly with that aspect ratio
 var xmlhttp = new XMLHttpRequest();
