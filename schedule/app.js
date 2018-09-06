c30b16655cf142f05ba9c53f1c5ba4b2c60362e3
KeystrokeCascade <35793455+KeystrokeCascade@users.noreply.github.com>
2018-09-06 23:08:20 +1000

    up next placeholder

--- schedule/app.js
+++ schedule/app.js
@@ -98,176 +98,310 @@ function toTimeZone(time, zone) {
 //Runs it when loaded and then every minute
 schedule();
 function timer() {
-  var seconds = new Date().getSeconds();
-  if (seconds == 0) {
-    schedule();
-  };
+	var seconds = new Date().getSeconds();
+	if (seconds == 0) {
+		schedule();
+	};
 };
 setInterval(timer,1000);
 
 //Takes the time and date and calculates the event on
 function schedule() {
-  
-  //Creates time and prints it
-  var time_output = toTimeZone(new Date(), 'Australia/Melbourne');
-  document.getElementById("time").textContent = time_output;
-  
-  //Converts the day of week from a number to string and prints it
-  var time = moment.tz(new Date(),'Australia/Melbourne');
-  var day = time.format('dddd');
-  document.getElementById("day").textContent = day;
-  
-  //Changes time to an integer
-  var time = time_output.replace(":", "");
-  time = parseInt(time, 10)
-  
-  //Chooses what time and day corrospond to the schedule
-  var agenda = ""
-  //Sunday
+	
+	//Creates time and prints it
+	var time_output = toTimeZone(new Date(), 'Australia/Melbourne');
+	document.getElementById("time").textContent = time_output;
 
-  if (day == "Sunday") {
-      if (time >= 0 && time <= 700) {
-        agenda = "sleeping"
-      } else if (time >= 701 && time <= 2200) {
-        agenda = "at home"
-      } else if (time >= 2201 && time <= 2400) {
-        agenda = "sleeping"
-      };
+	//Converts the day of week from a number to string and prints it
+	var time = moment.tz(new Date(),'Australia/Melbourne');
+	var day = time.format('dddd');
+	document.getElementById("day").textContent = day;
+	
+	//Changes time to an integer
+	var time = time_output.replace(":", "");
+	time = parseInt(time, 10);
+	
+	//gets the schedule and parses the outputs
+	var agenda_out = agendaGet(day, time);
+	var agenda = agenda_out[0];
+	var up_next = agenda_out[1];
+	var time_next = agenda_out[2];
+	
+	//gets the time until next task
+	
+	//prints the current agenda
+	document.getElementById("schedule").textContent = agenda;
+	document.getElementById("up_next").textContent = up_next;
+	document.getElementById("time_next").textContent = time_next.substr(0,5);
+};
+
+//Chooses what time and day correspond to the schedule
+function agendaGet(day, time) {
+	var agenda = ""
+	var up_next = ""
+	var time_next = ""
+
+//Sunday
+	if (day == "Sunday") {
+		if (time >= 0 && time <= 700) {
+			agenda = "sleeping"
+			up_next = "at home"
+			time_next = "07:00:00"
+		} else if (time >= 701 && time <= 2200) {
+			agenda = "at home"
+			up_next = "sleeping"
+			time_next = 22
+		} else if (time >= 2201 && time <= 2400) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = 31.45
+		};
 
 //Monday
-  } else if (day == "Monday") {
-      if (time >= 0 && time <= 745) {
-        agenda = "sleeping"
-      } else if (time >= 746 && time <= 845) {
-        agenda = "on the school bus"
-      } else if (time >= 846 && time <= 1115) {
-        agenda = "studying"
-      } else if (time >= 1116 && time <= 1135) {
-        agenda = "on break"
-      } else if (time >= 1136 && time <= 1305) {
-        agenda = "studying"
-      } else if (time >= 1306 && time <= 1350) {
-        agenda = "on break"
-      } else if (time >= 1351 && time <= 1520) {
-        agenda = "studying"
-      } else if (time >= 1521 && time <= 1620) {
-        agenda = "on the school bus"
-      } else if (time >= 1621 && time <= 2200) {
-        agenda = "at home"
-      } else if (time >= 2201 && time <= 2400) {
-        agenda = "sleeping"
-      };
+	} else if (day == "Monday") {
+		if (time >= 0 && time <= 745) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = 7.45
+		} else if (time >= 746 && time <= 845) {
+			agenda = "on the school bus"
+			up_next = "studying"
+			time_next = 8.45
+		} else if (time >= 846 && time <= 1115) {
+			agenda = "studying"
+			up_next = "on break"
+			time_next = 11.15
+		} else if (time >= 1116 && time <= 1135) {
+			agenda = "on break"
+			up_next = "studying"
+			time_next = 11.35
+		} else if (time >= 1136 && time <= 1305) {
+			agenda = "studying"
+			up_next = "on break"
+			time_next = 13.05
+		} else if (time >= 1306 && time <= 1350) {
+			agenda = "on break"
+			up_next = "studying"
+			time_next = 13.50
+		} else if (time >= 1351 && time <= 1520) {
+			agenda = "studying"
+			up_next = "on the school bus"
+			time_next = 15.20
+		} else if (time >= 1521 && time <= 1620) {
+			agenda = "on the school bus"
+			up_next = "at home"
+			time_next = 16.20
+		} else if (time >= 1621 && time <= 2200) {
+			agenda = "at home"
+			up_next = "sleeping"
+			time_next = 22
+		} else if (time >= 2201 && time <= 2400) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = 31.45
+		};
 
 //Tuesday
-  } else if (day == "Tuesday") {
-      if (time >= 0 && time <= 745) {
-        agenda = "sleeping"
-      } else if (time >= 746 && time <= 845) {
-        agenda = "on the school bus"
-      } else if (time >= 846 && time <= 1115) {
-        agenda = "studying"
-      } else if (time >= 1116 && time <= 1135) {
-        agenda = "on break"
-      } else if (time >= 1136 && time <= 1305) {
-        agenda = "studying"
-      } else if (time >= 1306 && time <= 1350) {
-        agenda = "on break"
-      } else if (time >= 1351 && time <= 1520) {
-        agenda = "studying"
-      } else if (time >= 1521 && time <= 1615) {
-        agenda = "on the school bus"
-      } else if (time >= 1616 && time <= 1745) {
-        agenda = "at work"
-      } else if (time >= 1746 && time <= 2200) {
-        agenda = "at home"
-      } else if (time >= 2201 && time <= 2400) {
-        agenda = "sleeping"
-      };
+	} else if (day == "Tuesday") {
+		if (time >= 0 && time <= 745) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = 7.45
+		} else if (time >= 746 && time <= 845) {
+			agenda = "on the school bus"
+			up_next = "studying"
+			time_next = 8.45
+		} else if (time >= 846 && time <= 1115) {
+			agenda = "studying"
+			up_next = "on break"
+			time_next = 11.45
+		} else if (time >= 1116 && time <= 1135) {
+			agenda = "on break"
+			up_next = "studying"
+			time_next = 11.35
+		} else if (time >= 1136 && time <= 1305) {
+			agenda = "studying"
+			up_next = "on break"
+			time_next = 13.05
+		} else if (time >= 1306 && time <= 1350) {
+			agenda = "on break"
+			up_next = "studying"
+			time_next = 13.50
+		} else if (time >= 1351 && time <= 1520) {
+			agenda = "studying"
+			up_next = "on the school bus"
+			time_next = 15.20
+		} else if (time >= 1521 && time <= 1615) {
+			agenda = "on the school bus"
+			up_next = "at work"
+			time_next = 16.15
+		} else if (time >= 1616 && time <= 1745) {
+			agenda = "at work"
+			up_next = "at home"
+			time_next = 17.45
+		} else if (time >= 1746 && time <= 2200) {
+			agenda = "at home"
+			up_next = "sleeping"
+			time_next = 22
+		} else if (time >= 2201 && time <= 2400) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = 31.45
+		};
 
 //Wednesday
-  } else if (day == "Wednesday") {
-      if (time >= 0 && time <= 745) {
-        agenda = "sleeping"
-      } else if (time >= 746 && time <= 845) {
-        agenda = "on the school bus"
-      } else if (time >= 846 && time <= 1115) {
-        agenda = "studying"
-      } else if (time >= 1116 && time <= 1135) {
-        agenda = "on break"
-      } else if (time >= 1136 && time <= 1230) {
-        agenda = "studying"
-      } else if (time >= 1231 && time <= 1450) {
-        agenda = "at VET"
-      } else if (time >= 1451 && time <= 1500) {
-        agenda = "on break"
-      } else if (time >= 1501 && time <= 1745) {
-        agenda = "at VET"
-      } else if (time >= 1745 && time <= 2200) {
-        agenda = "at home"
-      } else if (time >= 2201 && time <= 2400) {
-        agenda = "sleeping"
-      };
+	} else if (day == "Wednesday") {
+		if (time >= 0 && time <= 745) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = 7.45
+		} else if (time >= 746 && time <= 845) {
+			agenda = "on the school bus"
+			up_next = "studying"
+			time_next = 8.45
+		} else if (time >= 846 && time <= 1115) {
+			agenda = "studying"
+			up_next = "on break"
+			time_next = 11.15
+		} else if (time >= 1116 && time <= 1135) {
+			agenda = "on break"
+			up_next = "studying"
+			time_next = 11.35
+		} else if (time >= 1136 && time <= 1230) {
+			agenda = "studying"
+			up_next = "at VET"
+			time_next = 12.30
+		} else if (time >= 1231 && time <= 1450) {
+			agenda = "at VET"
+			up_next = "on break"
+			time_next = 14.50
+		} else if (time >= 1451 && time <= 1500) {
+			agenda = "on break"
+			up_next = "at VET"
+			time_next = 15
+		} else if (time >= 1501 && time <= 1745) {
+			agenda = "at VET"
+			up_next = "at home"
+			time_next = 17.45
+		} else if (time >= 1745 && time <= 2200) {
+			agenda = "at home"
+			up_next = "sleeping"
+			time_next = 22
+		} else if (time >= 2201 && time <= 2400) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = 31.45
+		};
 	  
 //Thursday
-  } else if (day == "Thursday") {
-      if (time >= 0 && time <= 745) {
-        agenda = "sleeping"
-      } else if (time >= 746 && time <= 845) {
-        agenda = "on the school bus"
-      } else if (time >= 846 && time <= 1115) {
-        agenda = "studying"
-      } else if (time >= 1116 && time <= 1135) {
-        agenda = "on break"
-      } else if (time >= 1136 && time <= 1305) {
-        agenda = "studying"
-      } else if (time >= 1306 && time <= 1350) {
-        agenda = "on break"
-      } else if (time >= 1351 && time <= 1520) {
-        agenda = "studying"
-      } else if (time >= 1521 && time <= 1620) {
-        agenda = "on the school bus"
-      } else if (time >= 1621 && time <= 2200) {
-        agenda = "at home"
-      } else if (time >= 2201 && time <= 2400) {
-        agenda = "sleeping"
-      };
+	} else if (day == "Thursday") {
+		if (time >= 0 && time <= 745) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = 7.45
+		} else if (time >= 746 && time <= 845) {
+			agenda = "on the school bus"
+			up_next = "studying"
+			time_next = 8.45
+		} else if (time >= 846 && time <= 1115) {
+			agenda = "studying"
+			up_next = "on break"
+			time_next = 11.15
+		} else if (time >= 1116 && time <= 1135) {
+			agenda = "on break"
+			up_next = "studying"
+			time_next = 11.35
+		} else if (time >= 1136 && time <= 1305) {
+			agenda = "studying"
+			up_next = "on break"
+			time_next = 13.05
+		} else if (time >= 1306 && time <= 1350) {
+			agenda = "on break"
+			up_next = "studying"
+			time_next = 13.50
+		} else if (time >= 1351 && time <= 1520) {
+			agenda = "studying"
+			up_next = "on the school bus"
+			time_next = 15.20
+		} else if (time >= 1521 && time <= 1620) {
+			agenda = "on the school bus"
+			up_next = "at home"
+			time_next = 16.20
+		} else if (time >= 1621 && time <= 2200) {
+			agenda = "at home"
+			up_next = "sleeping"
+			time_next = 22
+		} else if (time >= 2201 && time <= 2400) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = "07:45:00"
+		};
 
 //Friday
-  } else if (day == "Friday") {
-      if (time >= 0 && time <= 745) {
-        agenda = "sleeping"
-      } else if (time >= 746 && time <= 845) {
-        agenda = "on the school bus"
-      } else if (time >= 846 && time <= 1115) {
-        agenda = "studying"
-      } else if (time >= 1116 && time <= 1135) {
-        agenda = "on break"
-      } else if (time >= 1136 && time <= 1305) {
-        agenda = "studying"
-      } else if (time >= 1306 && time <= 1350) {
-        agenda = "on break"
-      } else if (time >= 1351 && time <= 1520) {
-        agenda = "studying"
-      } else if (time >= 1521 && time <= 1600) {
-        agenda = "on the school bus"
-      } else if (time >= 1601 && time <= 1820) {
-        agenda = "at work"
-      } else if (time >= 1821 && time <= 2200) {
-        agenda = "at home"
-      } else if (time >= 2201 && time <= 2400) {
-        agenda = "sleeping"
-      };
+	} else if (day == "Friday") {
+		if (time >= 0 && time <= 745) {
+			agenda = "sleeping"
+			up_next = "on the school bus"
+			time_next = 7.45
+		} else if (time >= 746 && time <= 845) {
+			agenda = "on the school bus"
+			up_next = "studying"
+			time_next = 8.45
+		} else if (time >= 846 && time <= 1115) {
+			agenda = "studying"
+			up_next = "on break"
+			time_next = 11.15
+		} else if (time >= 1116 && time <= 1135) {
+			agenda = "on break"
+			up_next = "studying"
+			time_next = 11.35
+		} else if (time >= 1136 && time <= 1305) {
+			agenda = "studying"
+			up_next = "on break"
+			time_next = 13.05
+		} else if (time >= 1306 && time <= 1350) {
+			agenda = "on break"
+			up_next = "studying"
+			time_next = 13.50
+		} else if (time >= 1351 && time <= 1520) {
+			agenda = "studying"
+			up_next = "on the school bus"
+			time_next = 15.20
+		} else if (time >= 1521 && time <= 1600) {
+			agenda = "on the school bus"
+			up_next = "at work"
+			time_next = 16
+		} else if (time >= 1601 && time <= 1820) {
+			agenda = "at work"
+			up_next = "at home"
+			time_next = 18.20
+		} else if (time >= 1821 && time <= 2200) {
+			agenda = "at home"
+			up_next = "sleeping"
+			time_next = 22
+		} else if (time >= 2201 && time <= 2400) {
+			agenda = "sleeping"
+			up_next = "at home"
+			time_next = 32
+		};
 
 //Saturday
-  } else if (day == "Saturday") {
-      if (time >= 0 && time <= 800) {
-        agenda = "sleeping"
-      } else if (time >= 701 && time <= 2330) {
-        agenda = "at home"
-      } else if (time >= 2331 && time <= 2400) {
-        agenda = "sleeping"
-      };
-  };
-  //prints the current agenda
-  document.getElementById("schedule").textContent = agenda;
+	} else if (day == "Saturday") {
+		if (time >= 0 && time <= 800) {
+			agenda = "sleeping"
+			up_next = "at home"
+			time_next = 8
+		} else if (time >= 701 && time <= 2330) {
+			agenda = "at home"
+			up_next = "sleeping"
+			time_next = 23
+		} else if (time >= 2331 && time <= 2400) {
+			agenda = "sleeping"
+			up_next = "at home"
+			time_next = 31
+		};
+	};
+	
+	var agenda_out = [agenda, up_next, time_next]
+	return agenda_out
 };
