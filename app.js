7dbe33895639b1f69e2c2b042771edc22e6b3808
KeystrokeCascade <35793455+KeystrokeCascade@users.noreply.github.com>
2018-10-23 10:55:40 +1100

    Countdown

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-//Blinks the underscore
-var underscore = "_"
-function blinker() {
-  if (underscore == "_") {
-    underscore = ""
-  } else {
-    underscore = "_"
+//sets date to countdown to
+var countDownDate = new Date("2019-10-19 00:00:00").getTime();
+//updates every second
+var x = setInterval(function() {
+  //gets todays date+time
+  var now = new Date().getTime();
+  //gets time between now and countdown date
+  var distance = countDownDate - now;
+  //defines days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //stops the clock at 0
+  if(distance<=0){
+    clearInterval(x);
   }
-  document.getElementById("underscore").textContent = underscore;
-}
-
-setInterval(blinker,440);
+  //prints the countdown
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = ('0' + hours).slice(-2);
+  document.getElementById("minutes").innerHTML = ('0' + minutes).slice(-2);
+  document.getElementById("seconds").innerHTML = ('0' + seconds).slice(-2);
+}, 1000);
