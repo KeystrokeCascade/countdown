c234ffb091a5ad4c348ecbcccf68358272c937b6
KeystrokeCascade <35793455+KeystrokeCascade@users.noreply.github.com>
2018-10-23 14:01:06 +1100

    Test

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //loads the dates from dates.txt
-function request() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "dates.txt", false);
-	xhttp.send();
-	return xhttp.responseText;
+
+function request(callback) {
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onload = function(){ // when the request is loaded
+		callback(httpRequest.responseText);// we're calling our method
+	};
+	httpRequest.open('GET', "dates.txt");
+	httpRequest.send();
 };
+
 var dates = request();
 console.log(dates);
 
@@ -12,22 +16,22 @@ console.log(dates);
 var countDownDate = new Date("2019-10-19 00:00:00").getTime();
 //updates every second
 var x = setInterval(function() {
-  //gets todays date+time
-  var now = new Date().getTime();
-  //gets time between now and countdown date
-  var distance = countDownDate - now;
-  //defines days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  //stops the clock at 0
-  if(distance<=0){
-    clearInterval(x);
-  }
-  //prints the countdown
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = ('0' + hours).slice(-2);
-  document.getElementById("minutes").innerHTML = ('0' + minutes).slice(-2);
-  document.getElementById("seconds").innerHTML = ('0' + seconds).slice(-2);
+	//gets todays date+time
+	var now = new Date().getTime();
+	//gets time between now and countdown date
+	var distance = countDownDate - now;
+	//defines days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	//stops the clock at 0
+	if(distance<=0){
+		clearInterval(x);
+	}
+	//prints the countdown
+	document.getElementById("days").innerHTML = days;
+	document.getElementById("hours").innerHTML = ('0' + hours).slice(-2);
+	document.getElementById("minutes").innerHTML = ('0' + minutes).slice(-2);
+	document.getElementById("seconds").innerHTML = ('0' + seconds).slice(-2);
 }, 1000);
